29d938667111a61a1624663bdf651ae7caf9271b
Anna Ek <annaek@mail.weber.edu>
2023-03-18 22:56:10 -0400

    added animation to the home page

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -9,11 +9,28 @@ let btns = btnContainer.getElementsByClassName("nav-link");
 
 // Loop through the buttons and add the active class to the current/clicked button
 for (let i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    let current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
+    btns[i].addEventListener("click", function () {
+        let current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
 }
 
 
+function reveal() {
+    let reveals = document.querySelectorAll(".reveal");
+  
+    for (let i = 0; i < reveals.length; i++) {
+      let windowHeight = window.innerHeight;
+      let elementTop = reveals[i].getBoundingClientRect().top;
+      let elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active1");
+      } else {
+        reveals[i].classList.remove("active1");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
