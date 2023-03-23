525d2e6b610d3bcb2a8fc7fc9ecef31a83c50621
Anna Ek <annaek@mail.weber.edu>
2023-03-23 19:27:16 -0400

    fixed menu button, fixed image direction at bai difference section

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,22 +1,3 @@
-//TODO - Your ES6 JavaScript code (if any) goes here
-import 'bootstrap';
-
-// Get the container element
-let btnContainer = document.getElementById("navbarNav");
-
-// Get all buttons with class="btn" inside the container
-let btns = btnContainer.getElementsByClassName("nav-link");
-
-// Loop through the buttons and add the active class to the current/clicked button
-for (let i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function () {
-        let current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace(" active", "");
-        this.className += " active";
-    });
-}
-
-
 function reveal() {
     let reveals = document.querySelectorAll(".reveal");
   
@@ -32,5 +13,6 @@ function reveal() {
       }
     }
   }
+
   
   window.addEventListener("scroll", reveal);
